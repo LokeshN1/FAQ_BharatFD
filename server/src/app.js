7d8df786d87f19193d97b49b4a647aff5f94790e
LokeshN1 <lokeshnegi399@gmail.com>
2025-02-01 16:19:01 +0530

    chore: restructure backend files into server/src

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,21 @@
-const express = require('express');
+import dotenv from 'dotenv';
+
+import express from 'express';
+
+dotenv.config({ path: './src/.env' });
+
+import mongoose from 'mongoose';
+import { connectDB } from "./lib/db.js";
+
+// console.log("Dotenv Loaded:", process.env);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT);
 
 // Middleware to parse JSON
 app.use(express.json());
+
 
 // A simple route to test the server
 app.get('/', (req, res) => {
@@ -12,4 +24,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
