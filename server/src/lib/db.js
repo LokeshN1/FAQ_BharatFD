975d4e4168dfd484713016e8e806155304e6e1e2
LokeshN1 <lokeshnegi399@gmail.com>
2025-02-01 19:30:02 +0530

    feat: Set up MongoDB connection and create FAQ model

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        console.log(process.env.MONGO_URL);
         const conn = await mongoose.connect(process.env.MONGO_URL);   // connect mongoose with our mongodb database
         console.log(`MongoDB Connected ${conn.connection.host}`);
     }
