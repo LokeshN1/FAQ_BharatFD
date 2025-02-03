11e97c302ce68b2adf76d9de66fbfae8c0e16b4e
LokeshN1 <lokeshnegi399@gmail.com>
2025-02-03 23:09:04 +0530

    feat(redis): add Redis caching integration for FAQ translations
    - Added Redis setup and caching logic for FAQ translations.
    - Updated the controllers to fetch and store FAQ translations in Redis.

--- server/src/app.js
+++ server/src/app.js
@@ -1,39 +1,35 @@
-import dotenv from 'dotenv';
-
-import express from 'express';
-
-dotenv.config({ path: './src/.env' });
-
-import mongoose from 'mongoose';
-import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+import faqRoutes from "./routes/faq.Routes.js";
+import { connectDB } from "./lib/db.js";
 
-
-import faqRoutes from './routes/faq.Routes.js'
-// console.log("Dotenv Loaded:", process.env);
+dotenv.config({ path: "./src/.env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-console.log(process.env.PORT);
 
-// Middleware to parse JSON
+
+
+// Middleware
 app.use(express.json());
-
-
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000",
     credentials: true,
-}));
+  })
+);
 
-// FAQ routes
-app.use('/api', faqRoutes);
+// Routes
+app.use("/api", faqRoutes);
 
-// A simple route to test the server
-app.get('/', (req, res) => {
-  res.send('Hello from the FAQ API!');
+app.get("/", (req, res) => {
+  res.send("Hello from the FAQ API!");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
+
